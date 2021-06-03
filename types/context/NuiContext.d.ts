36008b2cdf7525a69c641d8d5128879705453c07
Anton Stjernquist <antonstjernquist@gmail.com>
2021-06-03 08:16:57 +0200

    Updated types, main is in /dist

--- types/context/NuiContext.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { NuiEvent } from '../provider/NuiProvider';
-export interface NUIContext {
-    addHandler<T = unknown>(event: string, handler: (event: MessageEvent<NuiEvent<T>>) => void): void;
-    removeHandler(event: string): void;
-}
-export declare const NuiContext: import("react").Context<NUIContext>;
--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './provider/NuiProvider';
-export * from './context/NuiContext';
-export * from './hooks/useNuiEvent';
