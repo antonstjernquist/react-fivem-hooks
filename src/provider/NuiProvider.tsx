d10bdf811d7418eb99f0b0134ff12d6ad2b9f37f
Anton Stjernquist <antonstjernquist@gmail.com>
2022-08-07 17:17:08 +0200

    feat: debug logs & mb fix

--- src/provider/NuiProvider.tsx
+++ src/provider/NuiProvider.tsx
@@ -9,6 +9,7 @@ export interface NuiEvent<T> {
 }
 
 export interface NuiProviderProps {
+  debug?: boolean;
   children: ReactNode;
   context?: React.Context<NUIContext>;
   validateEvent?(event: MessageEvent<NuiEvent<unknown>>): boolean;
@@ -17,26 +18,39 @@ export interface NuiProviderProps {
 export const NuiProvider = (props: NuiProviderProps) => {
   const Context = props.context ?? NuiContext;
   const handlers = useRef<Record<string, CallableFunction[]>>({});
-  handlers.current = {};
+
+  const debug = (...args: unknown[]) => {
+    if (!props.debug) {
+      return;
+    }
+
+    console.debug(args);
+  };
 
   const addHandler = (
     event: string,
     handler: (event: MessageEvent) => void
   ) => {
+    debug('Adding handler for event', event);
     const existingHandlers = handlers.current[event] ?? [];
+    debug('Existing handlers', existingHandlers);
     handlers.current[event] = [...existingHandlers, handler];
   };
 
   const removeHandler = (event: string) => {
+    debug('Removing handler for event', event);
     delete handlers.current[event];
   };
 
   const eventHandler = (event: MessageEvent<NuiEvent<unknown>>) => {
+    debug('Handling event', event);
     if (props.validateEvent && !props.validateEvent(event)) {
+      debug('Returning! Event was invalid', event);
       return;
     }
 
     const { type } = event.data;
+    debug('Checking if we should handle event .. ', type);
 
     if (
       handlers.current &&
@@ -44,6 +58,7 @@ export const NuiProvider = (props: NuiProviderProps) => {
       handlers.current[type].length > 0
     ) {
       handlers.current[type].forEach(handler => {
+        debug('Running handler for', type);
         handler(event);
       });
     }
